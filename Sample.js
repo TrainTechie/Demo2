1902021310ca03d59b5a49fe98887e57eab4ea84
TrainTechie <dgokulprasath@gmail.com>
2023-08-16 12:39:59 +0530

    Anu commit

---
+++ Sample.js
@@ -0,0 +1,37 @@
+function add(a=10,b=10)
+{
+    console.log("a :"+a)
+    console.log("b :"+b)
+
+}
+let ans=add(40,50)
+console.log(ans)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.getElementById("str").innerHTML="HI"
+
+// let luvtd={
+//     genre: "Love",
+//     music:"U1",
+//     val: {
+//         music:"ARR",
+//         genre: "Action"
+//     }
+// }
+// console.log(luvtd.val.music)
+
+
+// let varun=[34,56,78,90];          //0,1,2,3
+// console.log(varun[3]+varun[2])
